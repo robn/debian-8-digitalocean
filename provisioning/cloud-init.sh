355aa58a4f3f0a6fb785e37d6752ded184d59883
Rob N ★ <robn@robn.io>
2019-05-29 22:20:37 +1000

    Get cloud-init up and running
    I dunno I guess

---
+++ provisioning/cloud-init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ex
+export PATH=/sbin:/usr/sbin:$PATH
+
+# need cloud-init 0.77, which is jessie-backports, but is a little bit awkward
+# to get at in this post-jessie world
+cat >/etc/apt/sources.list.d/backports.list <<EOF
+deb http://archive.debian.org/debian jessie-backports main
+EOF
+cat >/etc/apt/apt.conf.d/backports.conf <<EOF
+Acquire::Check-Valid-Until "false";
+EOF
+
+apt-get update
+
+debconf-set-selections <<EOF
+cloud-init  cloud-init/datasources  multiselect  AltCloud, CloudStack, ConfigDrive, Ec2, MAAS, OVF, GCE, None
+EOF
+DEBIAN_FRONTEND=noninteractive apt-get -t jessie-backports -y install cloud-init cloud-initramfs-growroot
+
+apt-get clean
+
+rm -f /etc/apt/sources.list.d/backports.list /etc/apt/apt.conf.d/backports.conf
+
+apt-get update
